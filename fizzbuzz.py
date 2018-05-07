4051dffe481ac2a209f651d90b0b1ce690782763
Fanxing Zhang <fanxing_zhang1@live.ca>
2018-05-07 18:06:24 -0400

    May 7, 2018

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ret = []
+        for i in range(1, n + 1):
+            s = ""
+            if i % 3 == 0:
+                s += "Fizz"
+            if i % 5 == 0:
+                s += "Buzz"
+            if len(s) == 0:
+                ret.append(str(i))
+            else: 
+                ret.append(s)
+        return ret
